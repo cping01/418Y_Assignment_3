a5cd99c26a2bc09bc143780adefa5619eab95d32
cping01 <104638213+cping01@users.noreply.github.com>
2022-10-08 18:20:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get("/", (req, res) => {
 	res.render("index", {
 		"title": "Biography", // change between quotes to change the title in the web page
 		"name": "Connor Hing", // change between quotes to change the name in the web page
-		"bio": "This is a test bio. Blah Blah Blah I hope this works.", // change between quotes to change the bio in the web page
+		"bio": "Hi! My name is Connor, and I am a junior at UAlbany studying Computer Science. I am a transfer student from Hudson Valley Community College, where I received my associate degree in Computer Information Systems in 2021. I am originally from Detroit, MI. My parent’s and I moved from Detroit to Stephentown, NY around 2008.", // change between quotes to change the bio in the web page
 		"image": "https://418yassignment3.s3.amazonaws.com/20200430_170229.jpg" // change between quotes to change the image in the web page
 	});
 });
